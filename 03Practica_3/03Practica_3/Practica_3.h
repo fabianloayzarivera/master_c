92ce04aa2172fe516c966297707b681206183e53
Floayza <fabianloayzarivera@gmail.com>
2017-10-24 13:45:28 +0200

    First commit Practica 3

---
+++ 03Practica_3/03Practica_3/Practica_3.h
@@ -0,0 +1,7 @@
+#pragma once
+
+char * returnCad(int index, char *table[]);
+char * returnCad2(int index, char *table[]);
+char *table[] = { "hello", "this", "is", "an", "array", "of", "words" };
+int index = 4;
+int index2 = 3;
