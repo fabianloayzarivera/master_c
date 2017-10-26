c3cc91776c79ab53c971cd8cd2f9863a3ee133b3
FABIAN IGNACIO LOAYZA RIVERA <fabian.loayza@live.u-tad.com>
2017-10-26 18:09:04 +0200

    changed the movement method for pig

--- 04Practica_4/04Practica_4/Practica.h
+++ 04Practica_4/04Practica_4/Practica.h
@@ -5,7 +5,7 @@ const int JAGUAR_SPEED_X = 2;
 const int JAGUAR_SPEED_Y = 0;
 
 const int PIG_SPEED_X = 1;
-const int PIG_SPEED_Y = 1;
+const int PIG_SPEED_Y = 0;
 
 const int DEER_SPEED_X = 2;
 const int DEER_SPEED_Y = 1;
@@ -13,5 +13,9 @@ const int DEER_SPEED_Y = 1;
 const int DONKEY_SPEED_X = 1;
 const int DONKEY_SPEED_Y = 2;
 
-void moveEntity(TEntity* jaguar);
+void moveJaguarEntity(TEntity *animal);
+void moveDeerEntity(TEntity *animal);
+void movePigEntity(TEntity *animal);
+void moveDonkeyEntity(TEntity *animal);
+
 void paintEntity(TEntity *jaguar);
